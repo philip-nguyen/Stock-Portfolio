5a4e49822c24b1a3d5ff7e72fac110ba72c6482b
Philip <philip.nguyen@sjsu.edu>
2020-12-10 21:39:28 -0800

    outputs stockinfo in text area in gui

--- InvestmentStrats/ethical_investing.py
+++ InvestmentStrats/ethical_investing.py
@@ -2,6 +2,81 @@
 import yfinance as yf
 import datetime
 
+"""
+    The invest function takes in an investment amount
+    and returns a string var
+"""
+def invest(investmentAmount):
+    # Ethical Investing - investing in companies that promote
+    # ethical practices and ethical products.
+    # Choices include prominent wind energy companies
+
+    # General Electric - electric company
+    stockSymbol1 = "NEE"
+    stockSymbol1 = stockSymbol1.upper()
+
+    # NextEra Energy Inc - generates electricity through wind, solar and natural gas
+    stockSymbol2 = "GE"
+    stockSymbol2 = stockSymbol2.upper()
+
+    # Northland Power Inc - diversified energy company with assets in wind, natural gas and solar
+    stockSymbol3 = "NPIFF"
+    stockSymbol3 = stockSymbol3.upper()
+
+    try:
+        print("-------------------------- Stock 1 --------------------------")
+        division = float(investmentAmount*0.6)
+        output = "Money division: (60%) $" + str(division) + "\n"
+        print("Generating data from yfinance...")
+        output += printStockInfo(stockSymbol1) + "\n"
+
+        print("-------------------------- Stock 2 --------------------------")
+        division = float(investmentAmount * 0.2)
+        output += "Money division: (20%) $" + str(division) + "\n"
+        print("Generating data from yfinance...")
+        output += printStockInfo(stockSymbol2) + "\n"
+
+        print("-------------------------- Stock 3 --------------------------")
+        division = float(investmentAmount * 0.2)
+        output += "Money division: (20%) $" + str(division) + "\n"
+        print("Generating data from yfinance...")
+        output += printStockInfo(stockSymbol3) + "\n"
+        output += "\n\n"
+
+    except:
+        print("-------------------------- ERROR: Invalid Ticker Symbol --------------------------")
+        return "Error in Ethical Investing :(\n"
+
+    return output
+
+"""
+    The printStockInfo function takes a string stockSymbol
+    and returns a string var
+"""
+def printStockInfo(stockSymbol):
+    stock = yf.Ticker(stockSymbol)
+
+    # Print the Company's name and ticker symbol
+    stockInfo = stock.info
+    output = f"{stockInfo['shortName']} ({stockSymbol})"
+
+    # Get the current stock value
+    stockCurrent = float(stockInfo['open'])
+
+    # Get the previous close value
+    stockPreviousClose = float(stockInfo['previousClose'])
+
+    # Calculate the current price, value change and percent change
+    valueChange = round(stockCurrent - stockPreviousClose, 2)
+    percentChange = round((valueChange / stockPreviousClose) * 100, 2)
+    changeDirection = "+" if (valueChange > 0) else ""
+    output += (f'\n${stockCurrent:3g} {changeDirection}{valueChange} ({changeDirection}{percentChange}%)\n')
+
+    weeklyTrend = stock.history(period="5d")
+    output += "Weekly Trend:\n"
+    output += weeklyTrend.to_string() + '\n'
+    print(weeklyTrend)
+    return output
 
 def getStockInfo(stockSymbol):
     # convert symbol to upper case
--- gui.py
+++ gui.py
@@ -1,12 +1,26 @@
 # PyQt5 widgets here
-from PyQt5.QtWidgets import (QLineEdit, QLabel, QWidget, QCheckBox,
+from PyQt5.QtWidgets import (QLineEdit, QLabel, QWidget, QCheckBox, QPlainTextEdit,
                              QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox)
+from PyQt5.QtGui import QFont
+from InvestmentStrats import (ethical_investing )
+import threading
 import sys
 
 class StockPortfolio(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.investmentLabel = QLabel(self)
+        self.investmentInput = QLineEdit(self)
+        self.EthicalCheckBox = QCheckBox(self)
+        self.GrowthCheckBox = QCheckBox(self)
+        self.IndexCheckBox = QCheckBox(self)
+        self.QualityCheckBox = QCheckBox(self)
+        self.ValueCheckBox = QCheckBox(self)
+        self.submitButton = QPushButton('Submit')
+        self.resetButton = QPushButton('Reset')
+        self.outputArea = QPlainTextEdit(self)
+        self.output = ''
         self.UI()
 
     def UI(self):
@@ -15,69 +29,120 @@ class StockPortfolio(QWidget):
         # win = QMainWindow()
         # set the dimensions and window title
         # setGeometry(X, Y, width, height)
-        self.setGeometry(400, 400, 300, 300)
+        self.setGeometry(50, 100, 640, 700)
         self.setWindowTitle("Stock Portfolio")
 
         # Add Label and LineEdit
-        investmentLabel = QLabel(self)
-        investmentLabel.setText("Amount ($):")
-        investmentLabel.setStyleSheet("color: green")
-        investmentLabel.adjustSize()
-        investmentInput = QLineEdit(self)
+        # investmentLabel = QLabel(self)
+        self.investmentLabel.setText("Amount ($):")
+        self.investmentLabel.setStyleSheet("color: green")
+        # self.investmentLabel.adjustSize()
+        # self.investmentInput = QLineEdit(self)
 
         inputBox = QVBoxLayout()
-        inputBox.addWidget(investmentLabel)
-        inputBox.addWidget(investmentInput)
+        inputBox.addWidget(self.investmentLabel)
+        inputBox.addWidget(self.investmentInput)
 
         # Checkboxes
-        EthicalCheckBox = QCheckBox(self)
-        EthicalCheckBox.setText("Ethical Investing")
-        EthicalCheckBox.setStyleSheet("color: green")
-        # EthicalCheckBox.stateChanged.connect(show)
-        GrowthCheckBox = QCheckBox(self)
-        GrowthCheckBox.setText("Growth Investing")
-        GrowthCheckBox.setStyleSheet("color: green")
-        IndexCheckBox = QCheckBox(self)
-        IndexCheckBox.setText("Index Investing")
-        IndexCheckBox.setStyleSheet("color: green")
-        QualityCheckBox = QCheckBox(self)
-        QualityCheckBox.setText("Quality Investing")
-        QualityCheckBox.setStyleSheet("color: green")
-        ValueCheckBox = QCheckBox(self)
-        ValueCheckBox.setText("Value Investing")
-        ValueCheckBox.setStyleSheet("color: green")
+
+        self.EthicalCheckBox.setText("Ethical Investing")
+        self.EthicalCheckBox.setStyleSheet("color: green")
+
+        self.GrowthCheckBox.setText("Growth Investing")
+        self.GrowthCheckBox.setStyleSheet("color: green")
+
+        self.IndexCheckBox.setText("Index Investing")
+        self.IndexCheckBox.setStyleSheet("color: green")
+
+        self.QualityCheckBox.setText("Quality Investing")
+        self.QualityCheckBox.setStyleSheet("color: green")
+
+        self.ValueCheckBox.setText("Value Investing")
+        self.ValueCheckBox.setStyleSheet("color: green")
 
         checkBoxes = QVBoxLayout()
-        inputBox.addWidget(EthicalCheckBox)
-        inputBox.addWidget(GrowthCheckBox)
-        inputBox.addWidget(IndexCheckBox)
-        inputBox.addWidget(QualityCheckBox)
-        inputBox.addWidget(ValueCheckBox)
+        checkBoxes.addWidget(self.EthicalCheckBox)
+        checkBoxes.addWidget(self.GrowthCheckBox)
+        checkBoxes.addWidget(self.IndexCheckBox)
+        checkBoxes.addWidget(self.QualityCheckBox)
+        checkBoxes.addWidget(self.ValueCheckBox)
 
         # Buttons for submit and reset
-        submitButton = QPushButton('Submit')
-        resetButton = QPushButton('Reset')
-        resetButton.clicked.connect(investmentInput.clear)
-        # submitButton.clicked.connect(self.showResult())
+
+        self.resetButton.clicked.connect(self.investmentInput.clear)
+        self.resetButton.clicked.connect(self.outputArea.clear)
+        self.submitButton.clicked.connect(self.computeStockPortfolio)
+        # self.submitButton.clicked.connect(self.outputArea.clear)
 
         endBox = QHBoxLayout()
-        endBox.addWidget(submitButton)
-        endBox.addWidget(resetButton)
+        endBox.addWidget(self.submitButton)
+        endBox.addWidget(self.resetButton)
+
+        resultBox = QVBoxLayout()
+        resultBox.addWidget(self.outputArea)
 
         # Put all widgets together
         engineForm = QVBoxLayout()
         engineForm.addLayout(inputBox)
-        # engineForm.addLayout(checkBoxes)
+        engineForm.addLayout(checkBoxes)
         engineForm.addLayout(endBox)
+        engineForm.addLayout(resultBox)
 
         self.setLayout(engineForm)
         # launch the simple PyQt window
         self.show()
 
-    def showResult(self):
-        msg = QMessageBox(self)
-        msg.setWindowTitle("Stock Choices")
-        msg.setText("Here's the stocks")
-        msgBox = QVBoxLayout()
-        msgBox.addWidget(msg)
-        retval = msg.exec_()
+    def computeStockPortfolio(self):
+
+        print("Ethical check box value: ", int(self.EthicalCheckBox.isChecked()))
+        try:
+            investmentAmount = int(self.investmentInput.text())
+            self.output = ''
+            if self.EthicalCheckBox.isChecked():
+                # Start thread to do yfinance API calls
+                thread = threading.Thread(target=self.setOutput, args=(ethical_investing.invest,))
+                thread.start()
+                thread.join()
+
+            if self.GrowthCheckBox.isChecked():
+                # TODO: change args to growth_investing.invest function
+                thread = threading.Thread(target=self.setOutput, args=(ethical_investing.invest,))
+                thread.start()
+                thread.join()
+
+            if self.IndexCheckBox.isChecked():
+                # TODO: change args to index_investing.invest function
+                thread = threading.Thread(target=self.setOutput, args=(ethical_investing.invest,))
+                thread.start()
+                thread.join()
+
+            if self.QualityCheckBox.isChecked():
+                # TODO: change args to quality_investing.invest function
+                thread = threading.Thread(target=self.setOutput, args=(ethical_investing.invest,))
+                thread.start()
+                thread.join()
+
+            if self.ValueCheckBox.isChecked():
+                # TODO: change args to value_investing.invest function
+                thread = threading.Thread(target=self.setOutput, args=(ethical_investing.invest,))
+                thread.start()
+                thread.join()
+
+        except:
+            print("ERROR :(")
+
+        self.showTextAreaResult(self.output)
+
+    def setOutput(self, investmentStrat):
+        investmentAmount = int(self.investmentInput.text())
+        # storing the function in a var
+        investmentOutput = investmentStrat(investmentAmount)
+        self.output += investmentOutput
+
+    def showTextAreaResult(self, output):
+        self.outputArea.insertPlainText(output)
+        self.outputArea.setFont(QFont('Monopace', 10, QFont.Monospace))
+
+        outputBox = QVBoxLayout()
+        outputBox.addWidget(self.outputArea)
+
