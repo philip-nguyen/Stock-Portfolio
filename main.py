f21f50d75075ec6173c16f098ee25953c7f9ccbb
Philip <philip.nguyen@sjsu.edu>
2021-01-25 21:37:50 -0800

    removed starter code from main

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ import gui
 import sys
 from PyQt5.QtWidgets import QApplication
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-
 # initialize
 app = QApplication(sys.argv)
 
